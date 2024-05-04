2df7196333610d38f14ff4c2a05a3535c21649e1
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2024-05-05 00:59:13 +0300

    add step 3

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -4,8 +4,8 @@ import { nanoid } from "nanoid";
 
 const contactsPath = path.resolve("db", "contacts.json");
 
-const updateContacts = (constants) =>
-  fs.writeFile(contactsPath, JSON.stringify(constants, null, 2));
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 export async function listContacts() {
   const contacts = await fs.readFile(contactsPath);
@@ -20,7 +20,7 @@ export async function getContactById(contactId) {
 
 export async function removeContact(contactId) {
   const contacts = await listContacts();
-  const index = contacts.findIndex((constant) => constant.id === contactId);
+  const index = contacts.findIndex((contact) => contact.id === contactId);
   if (index === -1) {
     return null;
   }
@@ -31,16 +31,27 @@ export async function removeContact(contactId) {
   return result;
 }
 
-export async function addContact(name, email, phone) {
+export async function addContact(contact) {
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
-    name: name,
-    email: email,
-    phone: phone,
+    ...contact,
   };
   contacts.push(newContact);
   await updateContacts(contacts);
 
   return newContact;
 }
+
+export const updateContact = async (contactId, data) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+
+  contacts[index] = { ...contacts[index], ...data };
+  await updateContacts(contacts);
+
+  return contacts[index];
+};
